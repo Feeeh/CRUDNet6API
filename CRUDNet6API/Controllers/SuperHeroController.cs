51b620f6e9cc61b67b540ef14398c01d6f060d8f
Felipe G. Brandão <feeeh@gf2.com.br>
2022-03-06 20:55:17 -0300

    Initial Commit :)

---
+++ CRUDNet6API/Controllers/SuperHeroController.cs
@@ -0,0 +1,51 @@
+﻿using CRUDNet6API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDNet6API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private static List<SuperHero> heroes = new List<SuperHero>
+            {
+                new SuperHero {
+                    Id = 1,
+                    Name = "Spider-man",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                },
+                new SuperHero
+                {
+                    Id = 2,
+                    Name = "Ironman",
+                    FirstName = "Tony",
+                    LastName = "Stark",
+                    Place = "Long Island"
+                }
+            };
+
+        [HttpGet("Get")]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            return Ok(heroes);
+        }
+
+        [HttpGet("Get/{id}")]
+        public async Task<ActionResult<SuperHero>> Get(int id)
+        {
+            var hero = heroes.Find(x => x.Id == id);
+            if (hero == null) return BadRequest("Super-heroi nao encontrado");
+            return Ok(hero);
+        }
+
+
+        [HttpPost("Add")]
+        public async Task<ActionResult<List<SuperHero>>> Add(SuperHero superHero)
+        {
+            heroes.Add(superHero);
+            return Ok(heroes);
+        }
+    }
+}
